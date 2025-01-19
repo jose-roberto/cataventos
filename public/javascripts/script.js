e083795b4e1adbac42d656ec23dc02b2286fd59f
Thiago <thiagozduarte@gmail.com>
2025-01-19 11:38:35 -0300

    navbar generalization

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,10 @@
-function navigation(path) {
-    
+function load_header() {
+  fetch('/views/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Erro ao carregar o cabeçalho:', error));
 }
+
+window.onload = load_header;
